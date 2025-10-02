059b6b61d01e1fccd4f5899ffb93eacc9b314ab1
garopoulos <garopoulos@arx.net>
2025-10-02 13:04:15 +0300

    Addition of a new method to get the Next available DSN

---
+++ manolo/base/data/service/ManoloDataTier.Api/Features/DataStructure/GetNextDataStructureNumber/GetNextDataStructureNumberQuery.cs
@@ -0,0 +1,6 @@
+﻿using ManoloDataTier.Logic.Domains;
+using MediatR;
+
+namespace ManoloDataTier.Api.Features.DataStructure.GetNextDataStructureNumber;
+
+public class GetNextDataStructureNumberQuery : IRequest<Result>;
